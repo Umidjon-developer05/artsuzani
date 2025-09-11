e985e052d9b8f085bba950e528c8415eb26c4a93
Umidjon-developer05 <umidjongafforov175@gmail.com>
2025-09-11 13:35:45 +0500

    :delete ModeToggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 ﻿import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/theme-provider";
 import Whatsapp from "@/components/shared/whatsapp";
 import "./globals.css";
 
@@ -18,17 +17,10 @@ export default async function LocaleLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-            <div className="relative z-50">
-              <Whatsapp />
-            </div>
-          </ThemeProvider>
+          {children}
+          <div className="relative z-50">
+            <Whatsapp />
+          </div>
         </body>
       </html>
     </ClerkProvider>
