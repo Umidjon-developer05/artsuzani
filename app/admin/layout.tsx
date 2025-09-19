972744a1f7235689d291e734a594cd57291037ba
Umidjon-developer05 <umidjongafforov175@gmail.com>
2025-09-19 10:47:30 +0500

    admin role

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,9 @@
-﻿import React from "react";
+﻿"use client";
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import useUser from "@/hooks/use-user";
+
 import SidebarDashboard from "./_components/sidebar-dashboard";
 import {
   SidebarInset,
@@ -15,7 +20,27 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 
-const layout = ({ children }: any) => {
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user === undefined) return; // loading paytida hech narsa qilma
+    if (user === null) {
+      router.replace("/"); // login qilinmagan bo‘lsa
+    } else if (!user.isAdmin) {
+      router.replace("/"); // oddiy user bo‘lsa
+    }
+  }, [!user, router]);
+
+  if (user === undefined) return <div>Loading...</div>; // hali yuklanyapti
+  if (!user?.isAdmin) return <div>Loading...</div>;
+  if (user === null || !user.isAdmin) return null; // redirect bo‘lib ketadi
+
   return (
     <SidebarProvider>
       <div className="flex h-screen w-screen overflow-hidden">
@@ -23,7 +48,7 @@ const layout = ({ children }: any) => {
         <div className="w-full">
           <SidebarInset className="w-full border-b">
             <header className="bg-background sticky top-0 flex justify-between shrink-0 items-center gap-2 border-b p-4">
-              <div className="flex  items-center gap-2">
+              <div className="flex items-center gap-2">
                 <SidebarTrigger className="-ml-1" />
                 <Separator className="mr-2 data-[orientation=vertical]:h-4" />
                 <Breadcrumb>
@@ -45,6 +70,4 @@ const layout = ({ children }: any) => {
       </div>
     </SidebarProvider>
   );
-};
-
-export default layout;
+}
